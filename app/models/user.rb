8192cc3394d2a31dc741ec2b8f226181196d5520
Hiruyan-J <hiruyan0428@gmail.com>
2025-09-18 08:44:33 +0900

    update: Userモデルにnameのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,5 +22,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
 end
 
