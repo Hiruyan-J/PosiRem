0a249bcdb9267f73dc2d5aabd0272bdde62ef1cb
Hiruyan-J <hiruyan0428@gmail.com>
2025-09-18 17:14:07 +0900

    update: 各ページのタイトル表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     base_title = "PosiRem!"
     title.present? ? "#{title} | #{base_title}" : base_title
   end
+
+  def page_top_title(title = nil)
+    render "shared/page_top_title", title: title
+  end
 end
