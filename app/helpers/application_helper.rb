15df5b8c33faf0d036b82ee0328ceeeee0ad24c6
Hiruyan-J <hiruyan0428@gmail.com>
2025-09-18 23:28:00 +0900

    update: フラッシュメッセージ・フォーム入力時エラーメッセージ表示の追加, 新規登録フォームにnameの入力フォーム追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
+  # DeviseなどのflashタイプをDaisyUIのクラス名に変換する
+  def flash_class_for(type)
+    case type.to_s
+    when "notice"
+      "success"
+    when "alert"
+      "error"
+    else
+      type.to_s
+    end
+  end
+
   def page_title(title = "")
     base_title = "PosiRem!"
     title.present? ? "#{title} | #{base_title}" : base_title
