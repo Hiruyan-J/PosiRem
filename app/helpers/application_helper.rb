b54c004c73ac89c1da1defbe888625524014ad39
Hiruyan-J <hiruyan0428@gmail.com>
2025-12-07 07:27:45 +0900

    update: 見出し生成コードの名称変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,8 +11,8 @@ module ApplicationHelper
     end
   end
 
-  def page_top_title(title = nil)
-    render "shared/page_top_title", title: title
+  def page_heading(title = nil)
+    render "shared/page_heading", title: title
   end
 
   def default_meta_tags
